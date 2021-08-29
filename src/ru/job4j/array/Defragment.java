6e60faf6c319b08afb42513e3f1f8cfb542de743
HmViper <mazurink@mail.ru>
2021-08-30 00:52:52 +0300

    6.8. Дефрагментация массива [#225]

---
+++ src/ru/job4j/array/Defragment.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                int point = index;
+                for (int indexNotNull = point + 1; indexNotNull < array.length; indexNotNull++) {
+                    if (array[indexNotNull] != null) {
+                        String temp = array[indexNotNull];
+                        array[point] = temp;
+                        array[indexNotNull] = null;
+                        break;
+                    }
+                }
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}
